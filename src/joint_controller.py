e6843620841db5a95b56bf0ef08d398202ed5edc
Jibrangit <jibran.university@gmail.com>
2021-04-23 16:52:24 -0400

    included read and write of data

--- src/joint_controller.py
+++ src/joint_controller.py
@@ -9,8 +9,10 @@ from gazebo_msgs.srv import ApplyJointEffort
 #This function will send the joint values with /gazebo/apply_joint_effort
 class PD_Controller:
     def __init__(self):
-        self.error = 3
+        self.error = 0
         self.old_error = 0
+        self.time = rospy.Time.now()
+
     
     def send_joint_efforts(self, effort):
         apply_effort = rospy.ServiceProxy("/gazebo/apply_joint_effort", ApplyJointEffort)
@@ -18,12 +20,26 @@ class PD_Controller:
         #message fields: "joint_name: 'joint2', effort: 10.0, start_time: secs: 0 nsecs: 0,duration: secs: 10 nsecs: 0"
 
     def PD(self, current, desired):
+        Kp = 0.03
+        Ku = Kp/0.6
+        Tu = 0.01
+        Kd = (3*Ku*Tu)/40
         self.error = desired - current
+
+        desired_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/desired.txt", "a")
+        desired_file.write(str(desired) + '\n')
+        desired_file.close()
+
+        current_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/current.txt", "a")
+        current_file.write(str(current) + '\n')
+        current_file.close()
+
         delta_error = self.old_error - self.error
-        calculated_effort = (0.6 * self.error) + (8 * delta_error)
-        print(self.error)
+        calculated_effort = (Kp * self.error) + (Kd * delta_error)
+        print(f'Desired: {desired} Current: {current} Error: {self.error} Delta_error {delta_error} Time {rospy.Time.now()} ')
         self.send_joint_efforts(calculated_effort)
         self.old_error = self.error
+        # self.osc_per = rospy.Time.now() - self.time
 
 #must implement a service to get a reference position for the last joint and get it to go there
 
@@ -31,6 +47,14 @@ class PD_Controller:
 #should be a subscriber
 
 if __name__ == '__main__':
+    desired_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/desired.txt", "r+")
+    current_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/current.txt", "r+")
+    
+    desired_file.truncate(0)
+    desired_file.close()
+    current_file.truncate(0)
+    current_file.close()
+
     rospy.init_node('joint_controller')
     position_reached = False
     pd_controller = PD_Controller()
@@ -45,7 +69,6 @@ if __name__ == '__main__':
         if(pd_controller.error > 0.005):
             print('Position reached!')
             break
-        rospy.sleep(0.1)
 
 
     
