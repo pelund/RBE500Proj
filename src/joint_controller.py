f25d5f754b74a27ff2ce67b77370e76df8cb92f9
pelund <pelund@wpi.edu>
2021-04-23 14:56:53 -0700

    changed k vals, changed print

--- src/joint_controller.py
+++ src/joint_controller.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import rospy
 import gazebo_msgs
 #Gazebo_msg documentation:http://docs.ros.org/en/jade/api/gazebo_msgs/html/index-msg.html
@@ -5,13 +6,13 @@ import gazebo_msgs
 from gazebo_msgs.srv import GetJointProperties
 from gazebo_msgs.srv import ApplyJointEffort
 
-
 #This function will send the joint values with /gazebo/apply_joint_effort
 class PD_Controller:
     def __init__(self):
         self.error = 0
         self.old_error = 0
         self.time = rospy.Time.now()
+        self.current = 10000000000
 
     
     def send_joint_efforts(self, effort):
@@ -20,23 +21,25 @@ class PD_Controller:
         #message fields: "joint_name: 'joint2', effort: 10.0, start_time: secs: 0 nsecs: 0,duration: secs: 10 nsecs: 0"
 
     def PD(self, current, desired):
-        Kp = 0.03
-        Ku = Kp/0.6
-        Tu = 0.01
-        Kd = (3*Ku*Tu)/40
+        self.current = current
+        Kp = .06
+        #Ku = Kp/0.6
+        #Tu = 0.01
+        #Kd = (3*Ku*Tu)/40
+        Kd = 0.1
         self.error = desired - current
 
-        desired_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/desired.txt", "a")
+        desired_file = open("/home/grystrion/catkin_ws/src/rbe_proj/src/desired.txt", "a")
         desired_file.write(str(desired) + '\n')
         desired_file.close()
 
-        current_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/current.txt", "a")
+        current_file = open("/home/grystrion/catkin_ws/src/rbe_proj/src/current.txt", "a")
         current_file.write(str(current) + '\n')
         current_file.close()
 
         delta_error = self.old_error - self.error
         calculated_effort = (Kp * self.error) + (Kd * delta_error)
-        print(f'Desired: {desired} Current: {current} Error: {self.error} Delta_error {delta_error} Time {rospy.Time.now()} ')
+        print(self.current)
         self.send_joint_efforts(calculated_effort)
         self.old_error = self.error
         # self.osc_per = rospy.Time.now() - self.time
@@ -47,8 +50,8 @@ class PD_Controller:
 #should be a subscriber
 
 if __name__ == '__main__':
-    desired_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/desired.txt", "r+")
-    current_file = open("/home/jibran/catkin_ws/src/rbe_proj/src/current.txt", "r+")
+    desired_file = open("/home/grystrion/catkin_ws/src/rbe_proj/src/desired.txt", "r+")
+    current_file = open("/home/grystrion/catkin_ws/src/rbe_proj/src/current.txt", "r+")
     
     desired_file.truncate(0)
     desired_file.close()
@@ -63,12 +66,14 @@ if __name__ == '__main__':
         current_joint_properties = joint_properties('joint_6')
         current_joint_position = current_joint_properties.position[0]
         # print(current_joint_position)
-        desired_joint_position = -0.5
+        desired_joint_position = 0
 
         pd_controller.PD(current_joint_position, desired_joint_position)
-        if(pd_controller.error > 0.005):
+        if((pd_controller.error > 0.005)and pd_controller.current > desired_joint_position-.005 and pd_controller.current < desired_joint_position+ .005):
             print('Position reached!')
-            break
+            #break
+        else:
+            print("Not there")
 
 
     
